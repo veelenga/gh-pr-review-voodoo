79faa9d2fc9e803901b191771211583e7dd1b3ed
Vitalii Elenhaupt <velenhaupt@gmail.com>
2019-09-08 20:43:30 +0300

    Initial commit (scaffolding)
    https://probot.github.io/docs/development/
    
    ```
    $ npx create-probot-app my-first-app
    ```

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { Application } from 'probot' // eslint-disable-line no-unused-vars
+
+export = (app: Application) => {
+  app.on('issues.labeled', async (context) => {
+    const issueComment = context.issue({ body: 'Thanks for opening this issue!!!' })
+    await context.github.issues.createComment(issueComment)
+  })
+}
