58c6a60baea2609d25b301a720ea7b9edc64b2a5
Vitalii Elenhaupt <velenhaupt@gmail.com>
2019-09-14 02:12:02 +0300

    getConfig

--- src/handler.ts
+++ src/handler.ts
@@ -5,18 +5,11 @@ import ReviewVoodoo, {
 
 
 export async function handlePullRequestLabelChange(context : Context): Promise<void> {
-  const config: AppConfig | null = await context.config<AppConfig | null>(
-    'pr-review-voodoo.yml'
-  )
-  if (!config) {
-    throw new Error('the configuration file failed to load')
-  }
-
   let payload = context.payload
   let label = { name: payload.label.name, action: payload.action }
   let reviewers = (payload.pull_request.request_reviewers || []).map((user: any) => user.login)
 
-  let voodoo = new ReviewVoodoo(config, label, reviewers);
+  let voodoo = new ReviewVoodoo(await getConfig(context), label, reviewers);
 
   voodoo.execute()
 
@@ -29,4 +22,18 @@ export async function handlePullRequestLabelChange(context : Context): Promise<v
     const params = context.issue({ reviewers: voodoo.reviewersToCreate })
     const result = await context.github.pullRequests.createReviewRequest(params)
   }
+}
+
+async function getConfig(context : Context): Promise<AppConfig> {
+  const config: AppConfig | null = await context.config<AppConfig | null>(
+    'pr-review-voodoo.yml'
+  )
+
+  if (!config) {
+    throw new Error('the configuration file failed to load')
+  }
+
+  config.minAmountOfReviewers = config.minAmountOfReviewers || 1
+  config.maxAmountOfReviewers = config.maxAmountOfReviewers || 2
+  return config
 }
--- src/review_voodoo.ts
+++ src/review_voodoo.ts
@@ -1,4 +1,6 @@
 export interface AppConfig {
+  minAmountOfReviewers: number
+  maxAmountOfReviewers: number
   reviewGroups: { [key: string]: string[] }
 }
 
